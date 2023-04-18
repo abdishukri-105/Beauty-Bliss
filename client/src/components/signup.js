ae1187f611a109367d7040a658cc4555a2371a31
Casey Ochieng <caseyochieng46@gmail.com>
2023-04-18 10:55:53 +0300

    switching

---
+++ client/src/components/signup.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import Navbar from "./Navbar";
+import AboutImg from "../assets/night.jpg";
+import Footer from "./Footer";
+
+
+function Product() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Product;
