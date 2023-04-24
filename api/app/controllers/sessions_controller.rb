6e5252400814d41d6731104b6438c6115954149f
GriffinsNgeno <117969990+GriffinsNgeno@users.noreply.github.com>
2023-04-24 11:57:52 +0300

    Update

--- api/app/controllers/sessions_controller.rb
+++ api/app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    def create_account
+    def create
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id
         render json: user
