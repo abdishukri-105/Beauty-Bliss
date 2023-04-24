971850075698b5106c103a4c0b9468b394f10e9e
m-njora <mmuthekijora@gmail.com>
2023-04-24 14:45:45 +0300

    modified slideshow

--- client/src/components/Slideshow.js
+++ client/src/components/Slideshow.js
@@ -1,24 +1,18 @@
 import React from 'react'
-// import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Card from './Card';
 
 function Slideshow() {
-    // const scrollLeft = () => {
-    //     document.getElementById("content").scrollLeft -= 400;
-    // }
-    // const scrollRight = () => {
-    //     document.getElementById("content").scrollLeft += 400;
-    // }
+    const scrollLeft = () => {
+        document.getElementById("content").scrollLeft -= 400;
+    }
+    const scrollRight = () => {
+        document.getElementById("content").scrollLeft += 400;
+    }
     
     return (
-        <div className="relative">
+        <div className="relative ">
         <div className=" font-['Poppins'] text-center py-4  text-2xl font-bold">Browse Our Products</div>
         <div className="absolute right-0 top-5 flex">
-          {/* <button onClick={scrollLeft} className="p-2 m-2 rounded-full bg-white">
-            <FontAwesomeIcon icon={faChevronLeft} style={{color: "#e8a0bf", border: "2px solid #e8a0bf "}} className="fa-2x fa-fw fa-border " />
-          </button> */}
         </div>
         <div id="content" className="carousel p-4 flex items-center justify-start overflow-x-auto scroll-smooth  scrollbar-hide">
           <div>
@@ -52,11 +46,23 @@ function Slideshow() {
             <Card />
           </div>
         </div>
-        {/* <button onClick={scrollRight} className="p-2 m-2 rounded-full bg-white">
-            <FontAwesomeIcon icon={faChevronRight} style={{color: "#e8a0bf", border: "2px solid #e8a0bf "}} className="fa-2x fa-fw fa-border" />
-          </button> */}
+    {/* <!-- Slider controls --> */}
+    <button onClick={scrollLeft} type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-pink dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
+            <span class="sr-only">Previous</span>
+        </span>
+    </button>
+    <button onClick={scrollRight} type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span className="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-pink dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg aria-hidden="true" className="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
 
-      </div>
+     </div>
+
+
     );
   }
   
