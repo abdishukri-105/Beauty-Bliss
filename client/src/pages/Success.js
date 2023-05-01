fc0bbaa4c91190710e4b085910f6f18cb6cbc3a3
m-njora <mmuthekijora@gmail.com>
2023-05-02 00:50:57 +0300

    fixed success message

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Success() {
     
     return (
-        <div class="fixed top-0 left-0 w-screen h-screen backdrop-filter backdrop-blur-xl">
+        <div class="fixed top-0 left-0 w-screen h-screen backdrop-filter bg-gray-50 backdrop-blur-sm">
         {/* <!-- Modal toggle --> */}
         {/* <div class="flex justify-center m-5">
                 <button id="successButton" data-modal-toggle="successModal" class="block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="button">
